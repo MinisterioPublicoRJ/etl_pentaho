99e5944ec3c00c66d2ff618eb754997c4d6f05fd
gabrielbacelar <gabrielbacelar@users.noreply.github.com>
2021-05-03 17:30:14 -0300

    itens classificar

--- painel_compras/painel_compras.bat
+++ painel_compras/painel_compras.bat
@@ -1,7 +1,7 @@
 REM passar -r para o python pra rodar esse script normal com agendamento todo dia as 13:30 pm
 cd "C:\pentaho\data-integration"
 c:
-Kitchen.bat /file:"E:\pentaho\etl\painel_compras\executa_painel_compras.kjb -param:PYTHON_PARAM=r"
+Kitchen.bat /file:"E:\pentaho\etl\painel_compras\executa_painel_compras.kjb" "-param:PYTHON_PARAM=r"
 
 REM para fornecer parâmetros para o job, acrescentar "-param:NOME_PARAM=VALOR_PARAM"
 REM ## IMPORTANTE ## é necessário acrescentar o parâmetro na tela de configuração do job com um valor default.
--- painel_compras/painel_compras_check_rows.bat
+++ painel_compras/painel_compras_check_rows.bat
@@ -1,7 +1,7 @@
 REM passar -c para o python pra checar se existe difenca de no numero de linhas das tabelas do gate, e rodar esse script com agendamento para toda hora
 cd "C:\pentaho\data-integration"
 c:
-Kitchen.bat /file:"E:\pentaho\etl\painel_compras\executa_painel_compras.kjb -param:PYTHON_PARAM=c"
+Kitchen.bat /file:"E:\pentaho\etl\painel_compras\executa_painel_compras.kjb" "-param:PYTHON_PARAM=c"
 
 REM para fornecer parâmetros para o job, acrescentar "-param:NOME_PARAM=VALOR_PARAM"
 REM ## IMPORTANTE ## é necessário acrescentar o parâmetro na tela de configuração do job com um valor default.
