c2eebfb9609c79749a44c68369a04525590a9684
gabrielbacelar <gabrielbacelar@users.noreply.github.com>
2021-06-09 17:20:23 -0300

    siga

--- painel_compras/python/painel_compras_fluxo.py
+++ painel_compras/python/painel_compras_fluxo.py
@@ -256,6 +256,22 @@ def check_table_checksum(configs, jndi_name, schema_name, table_name):
     return checksumid_changed
 
 
+def safe_column_string_to_datetime(dataframe, column_name):
+    return [None if not col_val or col_val == 'NaN' else col_val for col_val in dataframe[column_name]]
+
+
+def safe_column_string_to_int(dataframe, column_name):
+    return [None if not col_val or col_val == 'NaN' else int(round(float(col_val.replace(',', '.')))) for col_val in
+            dataframe[column_name]]
+
+
+def safe_column_string_to_float(dataframe, column_name):
+    dataframe[column_name] = [None if not col_val or col_val == 'NaN' else col_val.replace(',', '.') for col_val in
+                              dataframe[column_name]]
+    return [None if not col_val or col_val == 'NaN' else col_val.replace('.', '', col_val.count('.') - 1) for col_val in
+            dataframe[column_name]]
+
+
 def carga_painel_comprasrj():
     logger.info('carga_painel_comprasrj...')
     """
@@ -264,26 +280,98 @@ def carga_painel_comprasrj():
         para atualizar com novos registros;
     """
     db_opengeo = commons.get_database(configs.settings.JDBC_PROPERTIES[configs.settings.DB_OPENGEO_DS_NAME], api=None)
-    result_df_item_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-        configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_ItemContrato')['table']
-    result_df_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-        configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_Contrato')['table']
-    result_df_compra = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-        configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_Compra')['table']
-    server_encoding = dbcommons.show_server_encoding(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-        configs.settings.DB_OPENGEO_DS_NAME].jndi_name)
 
+    # origem - GATE
     # result_df_item_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-    #     configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='itens_contratos')['table']
+    #     configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_ItemContrato')['table']
     # result_df_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-    #     configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='contratos')['table']
+    #     configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_Contrato')['table']
     # result_df_compra = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
-    #     configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='outras_compras')['table']
+    #     configs.settings.DB_GATE_DS_NAME].jndi_name, schema_name='stage', table_name='ComprasRJ_Compra')['table']
     # server_encoding = dbcommons.show_server_encoding(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
     #     configs.settings.DB_OPENGEO_DS_NAME].jndi_name)
 
+    # origem - SIGA
+    result_df_item_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
+        configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='itens_contratos')[
+        'table']
+    result_df_item_contrato = result_df_item_contrato.rename(columns={
+        'contratacao': 'CONTRATACAO',
+        'id_item': 'ID_ITEM',
+        'item': 'ITEM',
+        'qtde_original': 'QTD',
+        'vl_unitoriginal': 'VL_UNIT',
+        'total_aditivadasuprimida': 'QTD_ADITIV_SUPR',
+        'vl_unitaditivadosuprimido': 'VL_UNIT_ADITIV_SUPR',
+        'dt_extracao': 'DT_EXTRACAO'
+    }).drop(['id', 'dt_ult_atualiz'], axis='columns')
+    result_df_item_contrato['VL_UNIT'] = safe_column_string_to_float(dataframe=result_df_item_contrato,
+                                                                     column_name='VL_UNIT')
+    result_df_item_contrato['QTD'] = safe_column_string_to_int(dataframe=result_df_item_contrato, column_name='QTD')
+    result_df_item_contrato['VL_UNIT_ADITIV_SUPR'] = safe_column_string_to_float(dataframe=result_df_item_contrato,
+                                                                                 column_name='VL_UNIT_ADITIV_SUPR')
+    result_df_item_contrato['QTD_ADITIV_SUPR'] = safe_column_string_to_int(dataframe=result_df_item_contrato,
+                                                                           column_name='QTD_ADITIV_SUPR')
+    result_df_contrato = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
+        configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='contratos')['table']
+    result_df_contrato = result_df_contrato.rename(columns={
+        'contratacao': 'CONTRATACAO',
+        'status_contratacao': 'STATUS',
+        'data_contratacao': 'DT_CONTRATACAO',
+        'unidade': 'ORGAO',
+        'processo': 'PROCESSO',
+        'objeto': 'OBJETO',
+        'tipo_de_aquisicao': 'TIPO_AQUISICAO',
+        'criterio_de_julgamento': 'CRITERIO_JULGAMENTO',
+        'data_inicio_vigencia': 'DT_INICIO',
+        'data_fim_vigencia': 'DT_FIM',
+        'fornecedor': 'FORNECEDOR',
+        'cpfcnpj': 'CPF_CNPJ',
+        'valor_total_contratovalor_estimado_para_contratacao_r': 'VL_ESTIMADO',
+        'valor_total_empenhado_r': 'VL_EMPENHADO',
+        'valor_total_liquidado_r': 'VL_EXECUTADO',
+        'valor_total_pago_r': 'VL_PAGO',
+        'dt_extracao': 'DT_EXTRACAO'
+    }).drop(['id', 'dt_ult_atualiz'], axis='columns')
+    result_df_contrato['DT_INICIO'] = safe_column_string_to_datetime(dataframe=result_df_contrato,
+                                                                     column_name='DT_INICIO')
+    result_df_contrato['DT_FIM'] = safe_column_string_to_datetime(dataframe=result_df_contrato, column_name='DT_FIM')
+    result_df_contrato['VL_ESTIMADO'] = safe_column_string_to_float(dataframe=result_df_contrato,
+                                                                    column_name='VL_ESTIMADO')
+    result_df_contrato['VL_EMPENHADO'] = safe_column_string_to_float(dataframe=result_df_contrato,
+                                                                     column_name='VL_EMPENHADO')
+    result_df_contrato['VL_EXECUTADO'] = safe_column_string_to_float(dataframe=result_df_contrato,
+                                                                     column_name='VL_EXECUTADO')
+    result_df_contrato['VL_PAGO'] = safe_column_string_to_float(dataframe=result_df_contrato, column_name='VL_PAGO')
+
+    result_df_compra = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
+        configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='compras_diretas')[
+        'table']
+    result_df_compra = result_df_compra.rename(columns={
+        'unidade': 'ORGAO',
+        'id_processo': 'ID_PROCESSO',
+        'processo': 'PROCESSO',
+        'objeto': 'OBJETO',
+        'afastamento': 'AFASTAMENTO',
+        'enquadramento_legal': 'ENQUADR_LEGAL',
+        'data_de_aprovacao': 'DT_APROVACAO',
+        'valor_do_processo_r': 'VL_PROCESSO',
+        'fornecedor_vencedor': 'FORNECEDOR',
+        'id_item': 'ID_ITEM',
+        'item': 'ITEM',
+        'qtd': 'QTD',
+        'unidade_de_medida': 'UNID',
+        'vl_unitario_r': 'VL_UNITARIO',
+        'dt_extracao': 'DT_EXTRACAO'
+    }).drop(['id', 'dt_ult_atualiz'], axis='columns')
+    result_df_compra['QTD'] = safe_column_string_to_int(dataframe=result_df_compra, column_name='QTD')
+    result_df_compra['VL_PROCESSO'] = safe_column_string_to_float(dataframe=result_df_compra, column_name='VL_PROCESSO')
+    result_df_compra['VL_UNITARIO'] = safe_column_string_to_float(dataframe=result_df_compra, column_name='VL_UNITARIO')
+
     result_df_catalogo = dbcommons.load_table(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
         configs.settings.DB_OPENGEO_DS_NAME].jndi_name, schema_name='comprasrj_stage', table_name='catalogo')['table']
+    server_encoding = dbcommons.show_server_encoding(configs=configs, jndi_name=configs.settings.JDBC_PROPERTIES[
+        configs.settings.DB_OPENGEO_DS_NAME].jndi_name)
 
     if isinstance(result_df_catalogo, pd.DataFrame) and not result_df_catalogo.empty:
         # carga catalogo
@@ -314,8 +402,8 @@ def carga_painel_comprasrj():
     if isinstance(result_df_item_contrato, pd.DataFrame) and not result_df_item_contrato.empty:
         # carga item_contrato
         values_for_fillna_item_contrato = {'QTD': 0, 'QTD_ADITIV_SUPR': 0, 'VL_UNIT': 0.0, 'VL_UNIT_ADITIV_SUPR': 0.0}
-        result_df_item_contrato = result_df_item_contrato.fillna(value=values_for_fillna_item_contrato).rename(
-            str.upper, axis='columns')
+        result_df_item_contrato = result_df_item_contrato.fillna(value=values_for_fillna_item_contrato)  # .rename(
+        #    str.upper, axis='columns')
         result_df_item_contrato['CHECKSUMID'] = commons.generate_checksum(result_df_item_contrato)
         result_df_item_contrato['un_item'] = unaccent_df(df=result_df_item_contrato, col='ITEM')
         result_df_item_contrato['DT_ULT_ATUALIZ'] = dt_now
@@ -342,8 +430,8 @@ def carga_painel_comprasrj():
     if isinstance(result_df_contrato, pd.DataFrame) and not result_df_contrato.empty:
         # carga contrato
         values_for_fillna_contrato = {'VL_ESTIMADO': 0.0, 'VL_EMPENHADO': 0.0, 'VL_EXECUTADO': 0.0, 'VL_PAGO': 0.0}
-        result_df_contrato = result_df_contrato.fillna(value=values_for_fillna_contrato).rename(str.upper,
-                                                                                                axis='columns')
+        result_df_contrato = result_df_contrato.fillna(value=values_for_fillna_contrato)  # .rename(str.upper,
+        #         axis='columns')
         result_df_contrato['CHECKSUMID'] = commons.generate_checksum(result_df_contrato)
         result_df_contrato['un_objeto'] = unaccent_df(df=result_df_contrato, col='OBJETO')
         result_df_contrato['DT_ULT_ATUALIZ'] = dt_now
@@ -366,8 +454,8 @@ def carga_painel_comprasrj():
     if isinstance(result_df_compra, pd.DataFrame) and not result_df_compra.empty:
         # carga compra
         values_for_fillna_compra = {'VL_UNITARIO': 0.0, 'VL_PROCESSO': 0.0, 'QTD': 0}
-        result_df_compra = result_df_compra.fillna(value=values_for_fillna_compra).rename(str.upper,
-                                                                                          axis='columns')
+        result_df_compra = result_df_compra.fillna(value=values_for_fillna_compra)  # .rename(str.upper,
+        #       axis='columns')
         result_df_compra['CHECKSUMID'] = commons.generate_checksum(result_df_compra)
         result_df_compra['un_objeto'] = unaccent_df(df=result_df_compra, col='OBJETO')
         result_df_compra['un_item'] = unaccent_df(df=result_df_compra, col='ITEM')
