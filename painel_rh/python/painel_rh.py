153fb021cf1fc47085126a44ac2077d5708ca55f
gabrielbacelar <gabrielbacelar@users.noreply.github.com>
2021-10-20 19:57:41 -0300

    etl do painel_rh

---
+++ painel_rh/python/painel_rh.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from datetime import datetime, timezone
+
+import mpmapas_commons as commons
+import mpmapas_logger
+import pandas as pd
+from db_utils import mpmapas_db_commons as dbcommons
+from mpmapas_exceptions import MPMapasException
+
+os.environ["NLS_LANG"] = ".UTF8"
+dt_now = datetime.now(timezone.utc)
+
+
+def main():
+    try:
+        logger.info('Starting %s.' % configs.settings.ETL_JOB)
+
+    except MPMapasException as c_err:
+        logger.exception(c_err)
+        exit(c_err.msg)
+    except Exception as c_err:
+        logger.exception('Fatal error in main')
+        exit(c_err)
+    finally:
+        logger.info('Finishing %s.' % configs.settings.ETL_JOB)
+
+
+global configs, logger
+
+if __name__ == '__main__':
+    try:
+        configs = commons.read_config('../etc/settings.yml')
+        mpmapas_logger.Logger.config_logger(configs, logghandler_file=True)
+        logger = logging.getLogger(configs.settings.ETL_JOB)
+
+        main()
+    except Exception as excpt:
+        logging.exception('Fatal error in %s' % __name__)
+        exit(excpt)
